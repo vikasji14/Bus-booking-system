3cf8d544608268af992d9f8bcb47f42ed5c1d817
vikasji14 <vikasdbg453@gmail.com>
2025-03-06 13:39:48 +0530

    'update'

--- server/server.js
+++ server/server.js
@@ -6,11 +6,23 @@ const port = process.env.PORT || 5000;
 const dbConfig = require("./config/dbConfig");
 const bodyParser = require("body-parser");
 
-app.use(cors());
+
+
+app.use(cors({
+  origin: 'https://bus-booking-system-1bk7.vercel.app', // Your frontend URL
+  methods: 'GET,POST,PUT,DELETE,OPTIONS',
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true, // Enable if you're using cookies or authentication
+}));
+
 app.use(express.json());
 app.use(bodyParser.json());
 
 
+
+app.get("/", (req, res) => {
+  res.send("Server is running...");
+});
 app.use("/api/users", require("./routes/usersRoutes"));
 app.use("/api/buses", require("./routes/busesRoutes"));
 app.use("/api/auth", require("./routes/authRoutes"));
