1b81f142052120dd809d3f65083d9b9b77d3d1e2
vikasji14 <vikasdbg453@gmail.com>
2025-03-06 15:42:26 +0530

    'ur'

--- client/src/helpers/axiosInstance.js
+++ client/src/helpers/axiosInstance.js
@@ -4,8 +4,5 @@ export const axiosInstance = axios.create({
   headers: {
     baseURL: "https://bus-booking-system-ed2p.vercel.app",
     Authorization: `Bearer ${localStorage.getItem("token")}`,
-    "Content-Type": "application/json",
-    withCredentials: true,
-
   },
 });
