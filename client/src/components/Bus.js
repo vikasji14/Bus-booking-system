95a4dd8696d865a5a489df50d9c90bb69830f927
githubinfovikas <vikasdbg453@gmail.com>
2025-03-06 22:23:38 +0530

    update

--- client/src/components/Bus.js
+++ client/src/components/Bus.js
@@ -14,14 +14,15 @@ function Bus({ bus }) {
           <p className="ml-2 font-base text-gray-500">{bus.journeyDate}</p>
         </div>
         <div className="mt-2 flex justify-start bg-white p-2"></div>
-        <div className="mt-2 flex sm:flex-row mx-6 sm:justify-between flex-wrap ">
-          <div className="flex flex-row place-items-center p-2">
-            <div className="flex flex-col ml-2">
-              <p className="text-base font-bold">{bus.name}</p>
+        <div className="mt-2 flex sm:flex-row flex-col justify-between flex-wrap ">
+
+          <div className=" p-2">
+            <div className=" ml-2">
+              <p className="text-base font-bold text-2xl">{bus.name}</p>
             </div>
           </div>
 
-          <div className="flex flex-col p-2">
+          <div className="flex flex-col flex-wrap p-2">
             <p className="font-bold">Departure Time</p>
             <p className="font-base">
               {moment(bus.departure, "HH:mm").format("hh:mm A")}
@@ -30,6 +31,7 @@ function Bus({ bus }) {
             <p className="font-bold">From </p>
             <p className="text-gray-500">{bus.from}</p>
           </div>
+
           <div className="flex flex-col flex-wrap p-2">
             <p className="font-bold">Arrival Time</p>
             <p className="font-base">
@@ -40,6 +42,7 @@ function Bus({ bus }) {
 
             <p className="text-gray-500">{bus.to}</p>
           </div>
+          
         </div>
         <div className="mt-4 bg-gray-100 flex flex-row flex-wrap md:flex-nowrap justify-between items-baseline">
           <div className="flex mx-6 py-4 flex-row flex-wrap">
@@ -59,8 +62,7 @@ function Bus({ bus }) {
             </svg>
 
             <div className="text-sm mx-2 flex flex-col">
-              <p className="font-bold text-base">Price</p>
-              <p className="font-base">{bus.price} DH</p>
+              <p className="font-bold text-base">Price ₹ {bus.price}/- </p>
             </div>
           </div>
           <div className="md:border-l-2 mx-6 md:border-dotted flex flex-row py-4 mr-6 flex-wrap">
