cd5713ac3b84649496d737d64177439e3f753b7f
githubinfovikas <vikasdbg453@gmail.com>
2025-03-06 11:50:48 +0530

    add

--- server/server.js
+++ server/server.js
@@ -6,21 +6,9 @@ const port = process.env.PORT || 5000;
 const dbConfig = require("./config/dbConfig");
 const bodyParser = require("body-parser");
 
-
-// ✅ Enable CORS
-app.use(cors({
-  origin: "*",  // Allow all origins (Use specific origin in production)
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allowed methods
-  allowedHeaders: ["Content-Type", "Authorization"] // Allowed headers
-}));
-
-// ✅ Handle Preflight (OPTIONS) Requests
-app.options("*", cors());
-
-
+app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
-
 
 
 app.use("/api/users", require("./routes/usersRoutes"));
