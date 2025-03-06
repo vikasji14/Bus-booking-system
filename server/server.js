3cb4b66ee1ef017dde14a7fa55fe0d3778f13f9e
githubinfovikas <vikasdbg453@gmail.com>
2025-03-06 19:25:43 +0530

    server

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const dbConfig = require("./config/dbConfig");
 const bodyParser = require("body-parser");
 
 app.use(cors({
-  origin:["https://bus-booking-system-1bk7.vercel.app", "http://localhost:3000","https://vercel.com/githubinfovikas-projects/bus-booking-system","https://vercel.com/githubinfovikas-projects"], // Allow frontend URL
+  origin:["https://bus-booking-system-1bk7.vercel.app", "http://localhost:3000"], // Allow frontend URL
   credentials: true, // Allow cookies & auth headers
   methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
   allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
