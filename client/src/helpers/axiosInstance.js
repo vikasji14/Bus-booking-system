969c78289a9ef3d02a031b1f9f3c0cc81d525033
vikasji14 <vikasdbg453@gmail.com>
2025-03-06 13:48:10 +0530

    'change'

--- client/src/helpers/axiosInstance.js
+++ client/src/helpers/axiosInstance.js
@@ -4,5 +4,10 @@ export const axiosInstance = axios.create({
   headers: {
     baseURL: "https://bus-booking-system-beige.vercel.app",
     Authorization: `Bearer ${localStorage.getItem("token")}`,
+    "Access-Control-Allow-Origin": "https://bus-booking-system-1bk7.vercel.app",
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+    "Access-Control-Allow-Headers": "Content-Type, Authorization",
+    withCredentials: true,
+
   },
 });
