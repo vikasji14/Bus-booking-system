95baffb8e694405550edaf19a69c1d4f2c2ea0f9
githubinfovikas <vikasdbg453@gmail.com>
2025-03-07 09:22:25 +0530

    change

--- client/src/pages/Admin/AdminBuses.jsx
+++ client/src/pages/Admin/AdminBuses.jsx
@@ -20,7 +20,7 @@ function AdminBuses() {
     try {
       setLoading(true);
       dispatch(ShowLoading());
-      const response = await axiosInstance.post(`http://localhost:5000/api/buses/get-all-buses`, {}, { withCredentials: true });
+      const response = await axiosInstance.post(`{process.env.REACT_APP_SERVER_URL}/api/buses/get-all-buses`, {}, { withCredentials: true });
       dispatch(HideLoading());
       if (response.data.success) {
         setBuses(response.data.data);
